c28aee326c2d6a186b8db6bfeedf287a6bbb51dd
Jordan Pangrazzi <jordan.pangrazzi@student.csn.edu>
2026-03-09 09:24:03 -0700

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -15,7 +15,7 @@ def response_generator():
         yield word + " "
         time.sleep(0.05)
 
-st.title("Simple chat")
+st.title("Jordan Pangrazzi's Chatbot")
 
 # Initialize chat history
 if "messages" not in st.session_state:
